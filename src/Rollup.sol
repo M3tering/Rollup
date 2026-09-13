5c0532d69c6ac7ecb8517219aa7634bf3fd4f8be
I. Christwin <iChristwin@gmail.com>
2026-09-13 12:56:53 +0100

    Update Solidity version and refactor state handling

--- src/Rollup.sol
+++ src/Rollup.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.37;
 
 import {LibRLP} from "solady@0.1.7/src/utils/LibRLP.sol";
 import {SSTORE2} from "solady@0.1.7/src/utils/SSTORE2.sol";
@@ -11,39 +11,42 @@ import {IRollup} from "./interfaces/IRollup.sol";
 contract Rollup is IRollup {
     bytes32 public anchorBlock;
     uint256 public chainLength;
-    address constant SP1_GROTH16_GATEWAY = 0x397A5f7f3dBd538f23DE225B51f532c34448dA9B;
-    bytes32 constant SP1_PROGRAM_VKEY = 0x005120317542200324c9509e78315ad70799268f02d21504709c8973d2493203; // ToDo: set to actual SP1 program vKey
+    bytes constant INCIPIT = hex"00";
 
     constructor() {
+        SSTORE2.write(INCIPIT);  //this contract nonce = 2x chainLength +1
+        SSTORE2.write(INCIPIT);  //this contract nonce = 2x chainLength +2
+        emit NewState(msg.sender, hex"", 0, INCIPIT, INCIPIT, INCIPIT);
         anchorBlock = blockhash(block.number - 1);
-        SSTORE2.write(hex"00");  //this contract nonce = 2x chainLength +1
-        SSTORE2.write(hex"00");  //this contract nonce = 2x chainLength +2
-        emit NewState(msg.sender, hex"", 0, hex"", hex"", hex"");
     }
 
     function commitState(bytes calldata accountBlob, bytes calldata nonceBlob, bytes calldata proof) external {
+        require(accountBlob.length == nonceBlob.length, InvalidBlobs());
+        require(accountBlob.length <= 24575, OversizeBlobs());
         // verifies proofs via SP1 Groth16 verifier gateway; reverts here if proof is invalid
         ISP1Verifier(SP1_GROTH16_GATEWAY)
             .verifyProof(
                 SP1_PROGRAM_VKEY, // ToDo: set to actual SP1 program vKey
                 bytes.concat(
                     anchorBlock, // ethereum state commitment
-                    stateAddress(chainLength, 0).codehash, // parent state commitment
-                    stateAddress(chainLength, 1).codehash, // parent state commitment
-                    hex"00",
-                    accountBlob, // proposed account state
-                    hex"00",
-                    nonceBlob // proposed nonce state
+                    stateAddress(chainLength, 0).codehash, // parent account-state commitment
+                    stateAddress(chainLength, 1).codehash, // parent nonce-state commitment
+                    INCIPIT,
+                    accountBlob, // proposed account-state
+                    INCIPIT,
+                    nonceBlob // proposed nonce-state
                 ),
                 proof
             );
 
-        chainLength++;
-        anchorBlock = blockhash(block.number - 1);
-        emit NewState(msg.sender, anchorBlock, chainLength, accountBlob, nonceBlob, proof);
         // Keep this order: exactly two CREATEs per committed state, account first.
         SSTORE2.write(accountBlob);  // this contract nonce = 2x chainLength +1 
         SSTORE2.write(nonceBlob);  // this contract nonce = 2x chainLength +2
+
+        chainLength++;
+        emit NewState(msg.sender, anchorBlock, chainLength, accountBlob, nonceBlob, proof);
+        anchorBlock = blockhash(block.number - 1);
+
     }
 
     function account(uint256 tokenId) external view returns (bytes6) {
@@ -60,12 +63,12 @@ contract Rollup is IRollup {
 
     function state(uint256 at, uint256 io, uint256 tokenId) public view returns (bytes6) {
         address pointer = stateAddress(at, io);
-        if (tokenId == 0) return bytes6(bytes.concat(hex"00", SSTORE2.read(pointer, 0, 5)));
-        uint256 index = (tokenId * 6) - 1;
-        return bytes6(SSTORE2.read(pointer, index, index + 6));
+        if (tokenId == 0) return bytes6(bytes.concat(INCIPIT, SSTORE2.read(pointer, 0, QUOTA-1)));
+        uint256 index = (tokenId * QUOTA) - 1;
+        return bytes6(SSTORE2.read(pointer, index, index + QUOTA));
     }
 
     function stateAddress(uint256 at, uint256 io) public view returns (address) {        
-        return LibRLP.computeAddress(address(this), at * 2 + 1 + io == 0 ? 0: 1);
+        return LibRLP.computeAddress(address(this), at * 2 + 1 + (io == 0 ? 0: 1));
     }
 }
